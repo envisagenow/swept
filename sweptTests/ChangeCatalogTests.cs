7ca3a28013f0721076f23199bbeb9285824cfad3
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2009-06-12 21:14:03 +0000

    A bit more honest coverage.
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@13 217a9c65-6828-9745-9b03-eb65f98e31ea

--- sweptTests/ChangeCatalogTests.cs
+++ sweptTests/ChangeCatalogTests.cs
@@ -1,6 +1,8 @@
 //  Swept:  Software Enhancement Progress Tracking.  Copyright 2008 Envisage Technologies, some rights reserved.
 //  This software is open source, under the terms of the MIT License.
 using NUnit.Framework;
+using NUnit.Framework.Constraints;
+using NUnit.Framework.SyntaxHelpers;
 using swept;
 using System.Collections.Generic;
 using System.Text;
@@ -21,18 +23,18 @@ namespace swept.Tests
         [Test]
         public void CanGetChangesFilteredByLanguage()
         {
-            cat.Add( new Change( "e1", "Fix using statements", FileLanguage.CSharp ) );
-            cat.Add( new Change( "e2", "Upgrade to XHTML", FileLanguage.HTML ) );
-            cat.Add( new Change( "e3", "Put <title> on all pages", FileLanguage.HTML ) );
+            cat.Add(new Change("e1", "Fix 'using' statements", FileLanguage.CSharp));
+            cat.Add(new Change("e2", "Upgrade to XHTML", FileLanguage.HTML));
+            cat.Add(new Change("e3", "Put <title> on all pages", FileLanguage.HTML));
 
-            List<Change> changes = cat.GetListForLanguage( FileLanguage.HTML );
-            Assert.AreEqual( 2, changes.Count );
-            Assert.AreEqual( "e2", changes[0].ID );
-            Assert.AreEqual( "e3", changes[1].ID );
+            List<Change> changes = cat.GetListForLanguage(FileLanguage.HTML);
+            List<Change> moreChanges = cat.FindAll(ch => ch.Language == FileLanguage.HTML);
+            Assert.IsTrue(changes.TrueForAll(ch => moreChanges.Contains(ch)));
+            Assert.AreEqual(moreChanges.Count, changes.Count);
 
-            changes = cat.GetListForLanguage( FileLanguage.CSharp );
-            Assert.AreEqual( 1, changes.Count );
-            Assert.AreEqual( "e1", changes[0].ID );
+            changes = cat.GetListForLanguage(FileLanguage.CSharp);
+            Assert.AreEqual(1, changes.Count);
+            Assert.AreEqual("e1", changes[0].ID);
         }
     }
 }
