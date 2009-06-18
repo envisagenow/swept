f21210b8ea7fe33175df22c7236326b4fa335cb6
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2009-06-18 15:58:12 +0000

    Update copyright block.  :D
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@18 217a9c65-6828-9745-9b03-eb65f98e31ea

--- sweptTests/ChangeWindowTests.cs
+++ sweptTests/ChangeWindowTests.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿//  Swept:  Software Enhancement Progress Tracking.  Copyright 2009 Envisage Technologies, some rights reserved.
+//  This software is open source, under the terms of the MIT License.
+//  The MIT License, roughly:  Keep this notice.  Beyond that, do whatever you want with this code.
+using System;
 using System.Collections.Generic;
 using System.Text;
 using NUnit.Framework;
