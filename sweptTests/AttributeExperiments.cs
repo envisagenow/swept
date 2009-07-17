c22d5cd59ffbfa4aaf38a6783cbf026f4dd62232
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2009-07-17 08:21:39 +0000

    Continue experiments with code attributes, towards [Exemplary] and [Woeful].
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@32 217a9c65-6828-9745-9b03-eb65f98e31ea

---
+++ sweptTests/AttributeExperiments.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace swept.Tests
+{
+    [TestFixture]
+    public class AttributeExperiments
+    {
+        [Test]
+        public void AttributeUsage_is_inherited_by_default()
+        {
+            var attr = new AttributeUsageAttribute(AttributeTargets.Method);
+            Assert.IsTrue(attr.Inherited);
+        }
+    }
+}
--- sweptTests/UnitTestQualityAttributes.cs
+++ sweptTests/UnitTestQualityAttributes.cs
@@ -19,25 +19,22 @@ namespace swept.Tests
     }
 
     //  For unit tests that are clear examples of the API, worthy of being a springboard.
-    [AttributeUsage(AttributeTargets.Method)]
     public class ExemplaryAttribute : UnitTestQualityAttribute
     {
         public ExemplaryAttribute(string reason) : base(reason) { }
     }
 
-    //  Is this gilding the lily?  How can something be notably unexciting?
-    //  It could at least show that we've considered it neither exemplary nor woeful.
-    [AttributeUsage(AttributeTargets.Method)]
-    public class MehAttribute : UnitTestQualityAttribute
-    {
-        public MehAttribute(string reason) : base(reason) { }
-    }
-
-    //  For unit tests we behave shadily in, to set up or validate, that we don't want to see in the app.
-    [AttributeUsage(AttributeTargets.Method)]
+    //  For when a test breaks expected normal usage to do its job.
     public class WoefulAttribute : UnitTestQualityAttribute
     {
         public WoefulAttribute(string reason) : base(reason) { }
+    }
+
+    //  Is this gilding the lily?  How can something be notably unexciting?
+    //  It could at least show that we've considered it neither exemplary nor woeful.
+    public class OkayAttribute : UnitTestQualityAttribute
+    {
+        public OkayAttribute(string reason) : base(reason) { }
     }
 
 }
