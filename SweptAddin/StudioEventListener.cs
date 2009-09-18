8b0587fd72fcd8c842dc1b1434a2b0ace47ab1a6
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2009-09-18 14:48:11 +0000

    Delousing much of the difficulty with connection.  Will require a re-drop of the testing.addin.
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@43 217a9c65-6828-9745-9b03-eb65f98e31ea

---
+++ SweptAddin/StudioEventListener.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EnvDTE80;
+using EnvDTE;
+
+namespace swept.Addin
+{
+    public class StudioEventListener
+    {
+        private DTE2 _studio;
+        private StudioAdapter _adapter;
+
+        public void Subscribe( DTE2 studio, swept.StudioAdapter adapter )
+        {
+            _studio = studio;
+            _adapter = adapter;
+
+            _studio.Events.SolutionEvents.Opened += new _dispSolutionEvents_OpenedEventHandler( Hear_SolutionOpened );
+
+            // TODO:  Subscribe the adapter to the rest of the Visual Studio IDE events we care about
+        }
+
+        public void Hear_SolutionOpened()
+        {
+            _adapter.RaiseSolutionOpened( _studio.Solution.FileName );
+        }
+
+    }
+}
