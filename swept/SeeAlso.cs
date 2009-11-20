2d2d5f0ccc977bb2cf41b1359177762d8fcb71a1
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2009-11-20 18:01:59 +0000

    With Ben Chesnut, more SeeAlso.
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@98 217a9c65-6828-9745-9b03-eb65f98e31ea

--- swept/SeeAlso.cs
+++ swept/SeeAlso.cs
@@ -5,13 +5,57 @@ using System.Text;
 
 namespace swept
 {
+    public enum TargetType
+    {
+        URL,
+        File,
+        SVN
+    }
+
     public class SeeAlso
     {
-        public SeeAlso() { }
+        public string Description { get; set; }
+
         public string Commit { get; set; }
+
+        private string _target;
+        public string Target 
+        {
+            get { return _target; }
+            set
+            {
+                _target = value;
+                string[] targetParts = value.Split(':' );
+                string prefix = string.Empty;
+                if (targetParts.Length > 0)
+                {
+                    prefix = targetParts[0].ToLower();
+                }
+
+                // TODO: svn can work over http protocol, so this won't be enough long-term.  move to deducing type attrib from Target label
+                switch (prefix)
+                {
+                    case "https":
+                    case "http":
+                        TargetType = TargetType.URL;
+                        break;
+                    case "file":
+                        TargetType = TargetType.File;
+                        break;
+                    case "svn":
+                        TargetType = TargetType.SVN;
+                        break;
+                    default:
+                        throw new ApplicationException( string.Format("Swept doesn't understand the TargetType of [{0}].", prefix) );
+                }
+            }
+        }
+        public TargetType TargetType { get; private set; }
+
+        public SeeAlso() { }
+        
         public string SVN { get; set; }
         public string ProjectFile { get; set; }
-        public string Description { get; set; }
         public string URL { get; set; }
     }
 
--- sweptTests/SeeAlsoTests.cs
+++ sweptTests/SeeAlsoTests.cs
@@ -17,17 +17,55 @@ namespace swept.Tests
         }
 
         [Test]
-        public void SeeAlso_holds_only_a_URL()
+        public void SeeAlso_holds_a_URLTarget()
         {
-            SeeAlso seeAlso = new SeeAlso {
+            SeeAlso seeAlso = new SeeAlso
+            {
                 Description = "See Spot run.",
-                URL = "Dev2000:8080/WhyToStringIsEvil"
+                Target = "http://Dev2000:8080/WhyToStringIsEvil"
             };
 
-            Assert.That( string.IsNullOrEmpty( seeAlso.ProjectFile ) );
-            Assert.That( string.IsNullOrEmpty( seeAlso.SVN ) );
-            Assert.That( string.IsNullOrEmpty( seeAlso.Commit ) );
-            Assert.That( seeAlso.URL == "Dev2000:8080/WhyToStringIsEvil" );
+            Assert.That( seeAlso.Target == "http://Dev2000:8080/WhyToStringIsEvil" );
+            Assert.That( seeAlso.TargetType == TargetType.URL );
+
+            seeAlso.Target = "https://Dev2000:8080/WhyToStringIsEvil";
+            Assert.That( seeAlso.TargetType == TargetType.URL );
+        }
+
+        [Test, ExpectedException( ExpectedMessage = "Swept doesn't understand the TargetType of [git]." )]
+        public void SeeAlso_unknown_protocol_throws()
+        {
+            SeeAlso seeAlso = new SeeAlso
+            {
+                Description = "super-l33t source control.",
+                Target = "git://totally/gplled/code"
+            };
+        }
+
+        [Test]
+        public void SeeAlso_holds_a_FileTarget()
+        {
+            SeeAlso seeAlso = new SeeAlso
+            {
+                Description = "This is how we want to do it now!",
+                Target = "file://./new_technology.cs"
+            };
+
+            Assert.That( seeAlso.Target == "file://./new_technology.cs" );
+            Assert.That( seeAlso.TargetType == TargetType.File );
+        }
+
+        [Test]
+        public void SeeAlso_holds_a_SVNTarget()
+        {
+            SeeAlso seeAlso = new SeeAlso
+            {
+                Description = "This is how we want to do it now!",
+                Target = "svn://somedirectory/changedClass.cs"
+            };
+
+            Assert.That( seeAlso.Target == "svn://somedirectory/changedClass.cs" );
+            Assert.That( seeAlso.TargetType == TargetType.SVN );
         }
 
         [Test]
