e00d7f925a65d94c7b276e57d5f758dc88f50bf3
coleoid <coleoid@217a9c65-6828-9745-9b03-eb65f98e31ea>
2012-06-23 19:34:06 +0000

    Straighten CoverageExcludeAttribute.
    git-svn-id: https://svn.code.sf.net/p/swept/code/swept/trunk@180 217a9c65-6828-9745-9b03-eb65f98e31ea

--- swept/CoverageExcludeAttribute.cs
+++ swept/CoverageExcludeAttribute.cs
@@ -1,10 +1,26 @@
-﻿using System;
+﻿//  Swept:  Software Enhancement Progress Tracking.
+//  Copyright (c) 2009, 2012 Jason Cole and Envisage Technologies Corp.
+//  This software is open source, MIT license.  See the file LICENSE for details.
+using System;
 
-/// <summary>This will exclude the tagged code from ncover coverage stats</summary>
+/// <summary>This excludes code from NCover coverage stats</summary>
 [CoverageExclude]
-public class CoverageExcludeAttribute: Attribute
+public class CoverageExcludeAttribute : Attribute
 {
-    public string Reason { get; private set; }
-    public CoverageExcludeAttribute() { }
-    public CoverageExcludeAttribute(string reason) : base() { Reason = reason; }
+    private readonly string _reason;
+
+    public CoverageExcludeAttribute() : base() { }
+
+    public CoverageExcludeAttribute(string reason)
+        : base()
+    {
+        _reason = reason;
+    }
+
+    public override string ToString()
+    {
+        return (_reason == null)? 
+            "Coverage Excluded."
+            : String.Format("Coverage Excluded: {0}", _reason);
+    }
 }
